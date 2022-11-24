7422cc18246a01e55cd02f6c39099e90032a08b7
ricardoescobarangel <ricardoescobarangel@gmail.com>
2022-11-24 17:47:56 -0500

    ejercicio typoFunction resuelto

--- src/typoFunction.js
+++ src/typoFunction.js
@@ -1,6 +1,7 @@
 // Escribe una funcion que tome un valor como argumento y devuelva el tipo de dato del argumento
 
 const typoFunction = (value) => {
+    return typeof value
 }
 
 export default typoFunction
