41ce741c36e30cea973fd2d3e0ea56acfb42dc01
cchavezmx <cchavezmx@outlook.com>
2022-11-24 18:50:01 -0600

    error en el problema de higherPrimerNumber

--- src/higherPrimeNumber.js
+++ src/higherPrimeNumber.js
@@ -1,6 +1,6 @@
 // Completa la funcion
 // Obten todos los numeros primos del numero que se pasa como argumento
-// Retorna el numero primo mas alto
+// Retorna el numero par mas alto
 
 const higherPrimeNumber = (number) => {
 
